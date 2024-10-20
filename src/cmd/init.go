82e2324dbe259bc5bff70f5a4ce6ab47072866aa
ryan <exogenesis@protonmail.com>
2024-10-20 13:47:45 +1100

    feat: Add database initialization functionality to the init command

--- src/cmd/init.go
+++ src/cmd/init.go
@@ -4,10 +4,14 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"database/sql"
 	_ "embed"
 	"fmt"
+	"log"
 
+	_ "github.com/lib/pq"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 //go:embed draftsmith.sql
@@ -21,21 +25,50 @@ var initCmd = &cobra.Command{
 
 This requires the database to be dropped first, use the drop command to do this.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
-        print(sql_commands)
+		fmt.Println("Initializing database...")
+
+		// Get database connection details from viper
+		dbHost := viper.GetString("db_host")
+		dbPort := viper.GetInt("db_port")
+		dbUser := viper.GetString("db_user")
+		dbPass := viper.GetString("db_pass")
+		dbName := viper.GetString("db_name")
+
+		// Create connection string
+		connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+			dbHost, dbPort, dbUser, dbPass, dbName)
+
+		// Open database connection
+		db, err := sql.Open("postgres", connStr)
+		if err != nil {
+			log.Fatalf("Error opening database connection: %v", err)
+		}
+		defer db.Close()
+
+		// Execute SQL commands
+		_, err = db.Exec(sql_commands)
+		if err != nil {
+			log.Fatalf("Error executing SQL commands: %v", err)
+		}
+
+		fmt.Println("Database initialized successfully.")
 	},
 }
 
 func init() {
 	cliCmd.AddCommand(initCmd)
 
-	// Here you will define your flags and configuration settings.
+	// Add flags for database connection details
+	initCmd.Flags().String("db_host", "localhost", "Database host")
+	initCmd.Flags().Int("db_port", 5432, "Database port")
+	initCmd.Flags().String("db_user", "postgres", "Database user")
+	initCmd.Flags().String("db_pass", "", "Database password")
+	initCmd.Flags().String("db_name", "draftsmith", "Database name")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Bind flags to viper
+	viper.BindPFlag("db_host", initCmd.Flags().Lookup("db_host"))
+	viper.BindPFlag("db_port", initCmd.Flags().Lookup("db_port"))
+	viper.BindPFlag("db_user", initCmd.Flags().Lookup("db_user"))
+	viper.BindPFlag("db_pass", initCmd.Flags().Lookup("db_pass"))
+	viper.BindPFlag("db_name", initCmd.Flags().Lookup("db_name"))
 }
